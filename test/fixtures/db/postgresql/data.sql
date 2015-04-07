8ed9dc6b7ee8a73ff694a9ed4d7298e483940f73
constantology <constantology@gmail.com>
2015-04-07 14:34:12 +0100

    fix test fixture dbs and their associated data

--- test/fixtures/db/postgresql/data.sql
+++ test/fixtures/db/postgresql/data.sql
@@ -1,1 +1,45 @@
--- test data inserts go here
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'SQL_ASCII';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = test, pg_catalog;
+
+--
+-- Data for Name: test; Type: TABLE DATA; Schema: test; Owner: test
+--
+
+COPY test (value, id) FROM stdin;
+your	1
+time	2
+is	3
+up	4
+I	5
+no	6
+longer	7
+want	8
+to	9
+play	10
+for	11
+you	12
+!	13
+\.
+
+
+--
+-- Name: test_id_seq; Type: SEQUENCE SET; Schema: test; Owner: test
+--
+
+SELECT pg_catalog.setval('test_id_seq', 13, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
--- test/fixtures/db/postgresql/schema.sql
+++ test/fixtures/db/postgresql/schema.sql
@@ -1,1 +1,78 @@
--- test schema go here
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'SQL_ASCII';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: test; Type: SCHEMA; Schema: -; Owner: test
+--
+
+CREATE SCHEMA test;
+
+
+ALTER SCHEMA test OWNER TO test;
+
+SET search_path = test, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: test; Type: TABLE; Schema: test; Owner: test; Tablespace: 
+--
+
+CREATE TABLE test (
+    value text,
+    id integer NOT NULL
+);
+
+
+ALTER TABLE test OWNER TO test;
+
+--
+-- Name: test_id_seq; Type: SEQUENCE; Schema: test; Owner: test
+--
+
+CREATE SEQUENCE test_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE test_id_seq OWNER TO test;
+
+--
+-- Name: test_id_seq; Type: SEQUENCE OWNED BY; Schema: test; Owner: test
+--
+
+ALTER SEQUENCE test_id_seq OWNED BY test.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: test; Owner: test
+--
+
+ALTER TABLE ONLY test ALTER COLUMN id SET DEFAULT nextval('test_id_seq'::regclass);
+
+
+--
+-- Name: test_pkey; Type: CONSTRAINT; Schema: test; Owner: test; Tablespace: 
+--
+
+ALTER TABLE ONLY test
+    ADD CONSTRAINT test_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
