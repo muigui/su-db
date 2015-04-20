0ea127e2404552774ff87bfb915e8feb4c6139d4
constantology <constantology@gmail.com>
2015-04-20 14:26:09 +0100

    tests for executing predefined queries

--- test/index.test.js
+++ test/index.test.js
@@ -5,10 +5,38 @@ var cothunkify = require('co-thunkify');
 
 var su_db = require(path.resolve());
 
+var dbs = require('./fixtures/db');
+
 var expect = chai.expect;
 
 suite('su-db', function() {
-    test('cristian is a...', cothunkify(function* () {
-        expect('guy').to.be.a.string;
+    var db;
+
+    suiteSetup(cothunkify(function* () {
+        db = yield su_db(dbs);
     }));
+
+    suite('mongodb', function() {
+        test('query', cothunkify(function* () {
+            var res = yield db.test_mongo.query();
+
+            expect(res).to.equal('your time is up I no longer want to play for you !');
+        }));
+    });
+
+    suite('postgresql', function() {
+        test('query', cothunkify(function* () {
+            var res = yield db.test_pg.query();
+
+            expect(res).to.equal('your time is up I no longer want to play for you !');
+        }));
+    });
+
+    suite('redis', function() {
+        test('query', cothunkify(function* () {
+            var res = yield db.test_redis.query('foo');
+
+            expect(res).to.equal('bar');
+        }));
+    });
 });
